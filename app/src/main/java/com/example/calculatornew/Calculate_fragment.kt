1b10374c098332fe116126350f927d20a18c3431
Vusala05 <167974345+Vusala05@users.noreply.github.com>
2024-08-11 16:54:53 +0400

    calculate_fragment.xml created

---
+++ app/src/main/java/com/example/calculatornew/Calculate_fragment.kt
@@ -0,0 +1,11 @@
+package com.example.calculatornew
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Calculate_fragment : Fragment(R.layout.fragment_calculate_fragment) {
+
+}
---
+++ app/src/main/java/com/example/calculatornew/Result_fragment.kt
@@ -0,0 +1,12 @@
+package com.example.calculatornew
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Result_fragment : Fragment(R.layout.fragment_result_fragment) {
+
+}
